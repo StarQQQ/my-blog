3233e556693e6c0749a2ba63b2e3dc830323483f
StarQQQ <33206427+StarQQQ@users.noreply.github.com>
2020-08-06 21:30:18 -0400

    first comment

---
+++ src/build/precache-manifest.e1ff34cad2cfc4732a11a2a8a49c7d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdc4e598326a06adf2efbbbba9bcfde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31161676bc28d33ae358",
+    "url": "/static/css/main.fe1e8706.chunk.css"
+  },
+  {
+    "revision": "079a04eb0793b50d1879",
+    "url": "/static/js/2.a0252a1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a0252a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31161676bc28d33ae358",
+    "url": "/static/js/main.208e3b13.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
